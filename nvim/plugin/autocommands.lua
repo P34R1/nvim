88d412a6a3d4303a92aab96b857dadcfc59b1a33
p34r1 <undeadgamer279@gmail.com>
2024-10-30 17:45:39 -0400

    only fmt run once

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -123,7 +123,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = function()
-        vim.lsp.buf.format { async = false }
+        vim.lsp.buf.format { async = false, id = ev.data.client }
       end,
     })
   end,
