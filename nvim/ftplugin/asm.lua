55c732d82ec850a5a92c2e04a17bb3686505d679
pearl <undeadgamer279@gmail.com>
2024-11-08 13:50:02 -0500

    add assembly lsp

---
+++ nvim/ftplugin/asm.lua
@@ -0,0 +1,22 @@
+if vim.fn.executable("asm-lsp") ~= 1 then
+  return
+end
+
+local root_files = {
+  'build.fish',
+  'justfile',
+  '.gitignore',
+  '.git',
+}
+
+vim.lsp.start {
+  name = 'asm-lsp',
+  cmd = { "asm-lsp" },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    nasm = {
+
+    },
+  },
+}
