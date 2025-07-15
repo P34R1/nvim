b75254a13ef700a5ad07014a547d07fa1d3c69f6
pearl <undeadgamer279@gmail.com>
2025-07-15 03:09:24 -0400

    add ziggy lsp

---
+++ nvim/ftplugin/ziggy_schema.lua
@@ -0,0 +1,25 @@
+if vim.fn.executable("ziggy") ~= 1 then
+  return
+end
+
+local root_files = {
+  '.git',
+  'build.zig',
+  '.gitignore',
+}
+
+vim.lsp.start {
+  name = 'ziggy schema lsp',
+  cmd = { "ziggy", "lsp", "--schema" },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    ziggy = {
+      -- analyses = {
+      --   unusedparams = true,
+      -- },
+      -- staticcheck = true,
+      -- gofumpt = true,
+    },
+  },
+}
